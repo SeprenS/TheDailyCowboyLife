94988cc86ce6a83ce47215acfc7d965a6c8f5b28
SeprenS <142746308+SeprenS@users.noreply.github.com>
2024-04-16 16:44:13 +0500

    new life

---
+++ Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace test
+{
+    partial class BallTrackingGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BallTrackingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 650);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "BallTrackingGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class BallTrackingGame : Form
+    {
+        private const int MaxBallSize = 50;
+        private const int MinBallSize = 1;
+        private int ballSize = MinBallSize;
+        private Point ballCenter;
+        private Random random = new Random();
+        private Timer gameTimer;
+        private Timer ballTimer;
+        private bool isGrowing = true;
+        private DateTime startTime;
+
+        public BallTrackingGame()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            ballCenter = GetRandomPosition();
+            startTime = DateTime.Now;
+            gameTimer = new Timer();
+            gameTimer.Interval = 60000;
+            gameTimer.Tick += (s, e) =>
+            {
+                gameTimer.Stop();
+                ballTimer.Stop();
+                MessageBox.Show("Game Over");
+            };
+            gameTimer.Start();
+            ballTimer = new Timer();
+            ballTimer.Interval = 20;
+            ballTimer.Tick += (s, e) =>
+            {
+                if (isGrowing)
+                {
+                    ballSize++;
+                    if (ballSize >= MaxBallSize)
+                    {
+                        isGrowing = false;
+                    }
+                }
+                else
+                {
+                    ballSize = MinBallSize;
+                    ballCenter = GetRandomPosition();
+                    isGrowing = true;
+                }
+
+                var elapsed = DateTime.Now - startTime;
+                ballTimer.Interval = (int)(25  * (1 + elapsed.TotalSeconds / 60));
+
+                Invalidate();
+            };
+            ballTimer.Start();
+
+            MouseClick += (s, e) =>
+            {
+                var ballRectangle = new Rectangle(new Point(ballCenter.X - ballSize / 2, ballCenter.Y - ballSize / 2), new Size(ballSize, ballSize));
+                if (ballRectangle.Contains(e.Location))
+                {
+                    ballSize = MinBallSize;
+                    ballCenter = GetRandomPosition();
+                    Invalidate();
+                }
+            };
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.FillEllipse(Brushes.Red, new Rectangle(new Point(ballCenter.X - ballSize / 2, ballCenter.Y - ballSize / 2), new Size(ballSize, ballSize)));
+        }
+
+        private Point GetRandomPosition()
+        {
+            return new Point(
+                random.Next(MaxBallSize, ClientSize.Width - MaxBallSize),
+                random.Next(MaxBallSize, ClientSize.Height - MaxBallSize));
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape) Close();
+        }
+    }
+}
